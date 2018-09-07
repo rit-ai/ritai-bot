ecf39695402e4b4b94aa744022db45a98b6bf3bf
Paul Galatic <pgalatic@gmail.com>
2018-09-07 15:37:17 -0400

    Add url validation and basic slackbot functionality

---
+++ bot.py
@@ -0,0 +1,143 @@
+# author: Paul Galatic
+# 
+# boilerplate code from:
+#   fullstackpython.com
+#   https://github.com/mattmakai
+
+import os
+import re
+import time
+import requests
+import urllib
+import validators
+from PIL import Image
+from io import BytesIO
+from slackclient import SlackClient
+
+# instantiate Slack client
+slack_client = SlackClient(os.environ.get('APP_BOT_USER_TOKEN'))
+# starterbot's user ID in Slack: value is assigned after the bot starts up
+starterbot_id = None
+
+# constants
+RTM_READ_DELAY = 1 # second delay between reading from RTM
+KMEANS_COMMAND = "kmeans"
+EXAMPLE_COMMAND = "do"
+MENTION_REGEX = "^<@(|[WU].+?)>(.*)"
+
+def check_url(url):
+    """
+    Returns True if the url returns a response code between 200-300,
+    otherwise return False.
+
+    author: Matto Todd
+    """
+    headers={
+            "Range": "bytes=0-10",
+            "User-Agent": "MyTestAgent",
+            "Accept":"*/*"
+    }
+
+    try:
+        response = urllib.request.urlopen(url)
+        return response.code in range(200, 300)
+    except urllib.error.URLError:
+        return False
+
+def parse_bot_commands(slack_events):
+    """
+    Parses a list of events coming from the Slack RTM API to find bot commands.
+    If a bot command is found, this function returns a tuple of command and 
+    channel. If its not found, then this function returns None, None.
+    """
+    for event in slack_events:
+        if event["type"] == "message" and not "subtype" in event:
+            user_id, message = parse_direct_mention(event["text"])
+            if user_id == starterbot_id:
+                return message, event["channel"]
+    return None, None
+
+def parse_direct_mention(message_text):
+    """
+    Finds a direct mention (a mention that is at the beginning) in message text
+    and returns the user ID which was mentioned. If there is no direct mention, 
+    returns None
+    """
+    matches = re.search(MENTION_REGEX, message_text)
+    # the first group contains the username
+    # the second group contains the remaining message
+    if matches:
+        return (matches.group(1), matches.group(2).strip())
+    else:
+        return (None, None)
+
+def handle_command(command, channel):
+    """
+    Executes bot command if the command is known
+    """
+    # Default response is help text for the user
+    default_response = "Unknown command. Try *{}*.".format(EXAMPLE_COMMAND)
+
+    # Finds and executes the given command, filling in response
+    response = None
+    # This is where you start to implement more commands!
+    if command.startswith(KMEANS_COMMAND):
+        bot_kmeans(command, channel)
+        repsonse = "Processed Kmeans command"
+
+    # Sends the response back to the channel
+    respond(response, channel)
+
+def respond(message, channel):
+    slack_client.api_call(
+        "chat.postMessage",
+        channel=channel,
+        text=message
+    )
+
+def bot_kmeans(command, channel):
+    command_list = command.split(' ')
+    # make sure all command arguments are present with no extras
+    if len(command_list) != 3:
+        respond('Usage: @ritai kmeans [image_url] [k_value]', channel)
+        return
+
+    img_url = command_list[1]
+    k_value = command_list[2]
+    
+    # validate url and k-value
+    print(check_url(img_url))
+    respond('Could not validate url. Are you sure it is correct?', channel)
+    return
+    if not (0 < k_value and k_value < 11):
+        respond('K values must be between 1 and 10 inclusive.', channel)
+        return
+
+    # acquire image
+    response = requests.get(img_url)
+    img = Image.open(BytesIO(response.content))
+
+    # TODO edit image here...
+
+    slack_client.api_call(
+        'files.upload',
+        channel=channel,
+        filename='out.png',
+        title='output',
+        initial_comment=('k: %d' % k_value),
+        file=img
+    )
+
+
+if __name__ == "__main__":
+    if slack_client.rtm_connect(with_team_state=False):
+        print("Starter Bot connected and running!")
+        # Read bot's user ID by calling Web API method `auth.test`
+        starterbot_id = slack_client.api_call("auth.test")["user_id"]
+        while True:
+            command, channel = parse_bot_commands(slack_client.rtm_read())
+            if command:
+                handle_command(command, channel)
+            time.sleep(RTM_READ_DELAY)
+    else:
+        print("Connection failed. Exception traceback printed above.")
---
+++ kmeans.py
@@ -0,0 +1,65 @@
+import numpy as np
+from scipy import misc
+
+def closest_centroids(points, centroids):
+    # get differences between each point
+    differences = points - centroids[:, np.newaxis, np.newaxis]
+    squareDifferences = differences**2
+    summedSquaredDifferences = squareDifferences.sum(axis=3)
+    finalDistances = np.sqrt(summedSquaredDifferences)
+    return np.argmin(finalDistances, axis=0)
+
+def move_centroids(points, closest, centroids):
+    newCentroids = []
+    for i in range(centroids.shape[0]):
+        # find which points are in group i
+        indices = (closest == i)
+        # get all points that are in group i
+        correspondingPoints = points[indices]
+        # average rgb values
+        average = correspondingPoints.mean(axis=0)
+        # add to new centroids list
+        newCentroids.append(average)
+
+    return np.array(newCentroids)
+
+def initialize_centroids(points, k):
+    (x, y, z) = points.shape
+    centroids = points.copy().reshape(x * y, z)
+    np.random.shuffle(centroids)
+    return centroids[:k]
+
+def set_to_centroids(points, centroids, closestCentroids):
+    newPoints = np.zeros(points.shape)
+    (x, y) = closestCentroids.shape
+    for i in range(x):
+        for j in range(y):
+            newPoints[i][j] = centroids[closestCentroids[i][j]]
+
+    return newPoints
+
+def kMeans(points, k, maxIter=10):
+    # init centroids randomly
+    centroids = initialize_centroids(points, k)
+
+    for i in range(0, maxIter):
+        print("Iteration: " + str(i + 1))
+        closestCentroids = closest_centroids(points, centroids)
+        centroids = move_centroids(points, closestCentroids, centroids)
+
+    finalPoints = set_to_centroids(points, centroids, closestCentroids)
+    return finalPoints
+
+def main():
+    # get input
+    imageName = input("Please enter image name: ")
+    points = misc.imread(imageName) # width x height x 3 array
+    k = int(input("Please enter k: ")) # number of groups
+
+    # copy points and save them
+    newPoints = kMeans(points, k)
+
+    # same new pixels to new image
+    misc.imsave("outfile.jpg", newPoints)
+
+main()
