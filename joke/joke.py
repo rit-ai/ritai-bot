ab86618f931669f8bca4a569dbfa61feb8b82d9f
Paul Galatic <pgalatic@gmail.com>
2018-09-24 16:55:48 -0400

    Added minimum "joke" length

--- joke/joke.py
+++ joke/joke.py
@@ -8,7 +8,21 @@ import random
 import numpy as np
 from pymarkovchain import MarkovChain
 
-def main():
+MIN_LENGTH = 50
+
+def joke():
+    if not os.path.isfile('markov.pkl'):
+        generate_database()
+    
+    chain = MarkovChain('./markov.pkl')
+    generated_joke = ''
+
+    while len(generated_joke) < MIN_LENGTH:
+        generated_joke = chain.generateString()
+
+    return generated_joke
+
+def generate_database():
     chain = MarkovChain('./markov.pkl')
 
     with open('in.txt') as f:
@@ -18,4 +32,4 @@ def main():
     print(chain.generateString())
 
 if __name__ == '__main__':
-    main()
+    print(joke())
